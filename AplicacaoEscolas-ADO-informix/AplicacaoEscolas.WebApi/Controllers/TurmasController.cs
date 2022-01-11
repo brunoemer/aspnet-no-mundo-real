26244926fbff43239c3a6e21dce0821ed41a2463
Bruno Emer <bruno.emer@tramontina.com>
2022-01-10 21:04:41 -0300

    Versao com conexao para informix

---
+++ AplicacaoEscolas-ADO-informix/AplicacaoEscolas.WebApi/Controllers/TurmasController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AplicacaoEscolas.WebApi.Infraestrutura;
+using AplicacaoEscolas.WebApi.Models;
+
+namespace AplicacaoEscolas.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TurmasController : ControllerBase
+    {
+        private readonly TurmasRepositorioSqlServer _turmasRepositorioSqlServer;
+
+        public TurmasController(TurmasRepositorioSqlServer turmasRepositorioSqlServer)
+        {
+            _turmasRepositorioSqlServer = turmasRepositorioSqlServer;
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar([FromBody]Turma turma)
+        {
+            turma.Id = Guid.NewGuid();
+            _turmasRepositorioSqlServer.Inserir(turma);
+
+            return Ok(turma);
+        }
+
+        [HttpGet]
+        public IActionResult RecuperarTodos([FromQuery]string descricao)
+        {
+            return Ok(_turmasRepositorioSqlServer.RecuperarTodos());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult RecuperarPorId(string id)
+        {
+
+            var guid = Guid.Parse(id);
+
+            return Ok(guid);
+        }
+
+
+    }
+}
---
+++ AplicacaoEscolas-ADO-informix/AplicacaoEscolas.WebApi/Infraestrutura/TurmasRepositorioSqlServer.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AplicacaoEscolas.WebApi.Models;
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+using IBM.Data.DB2.Core;
+
+namespace AplicacaoEscolas.WebApi.Infraestrutura
+{
+    public sealed class TurmasRepositorioSqlServer
+    {
+        private readonly IConfiguration _configuracao;
+
+
+        public TurmasRepositorioSqlServer(IConfiguration configuracao)
+        {
+            _configuracao = configuracao;
+        }
+
+        public void Inserir(Turma turma)
+        {
+            using (DB2Connection connection = new DB2Connection(_configuracao.GetConnectionString("EscolasIfx")))
+            {
+                var comando = new DB2Command(
+                    $"INSERT INTO Turmas (Id, Descricao) VALUES ('{turma.Id}','{turma.Descricao}')", connection);
+                connection.Open();
+                var resutlado = comando.ExecuteNonQuery();
+            }
+        }
+
+        public IEnumerable<Turma> RecuperarTodos()
+        {
+            using (DB2Connection connection = new DB2Connection(_configuracao.GetConnectionString("EscolasIfx")))
+            {
+                var comando = new DB2Command(@"SELECT Turmas.Id, Turmas.Descricao                                                 
+                                                        FROM Turmas"
+                    , connection);
+                connection.Open();
+                var reader = comando.ExecuteReader();
+                var listaTurmas = new List<Turma>();
+                while (reader.Read())
+                {
+
+                    listaTurmas.Add(
+                        new Turma()
+                        {
+                            Id = Guid.Parse(reader.GetString(0)),
+                            Descricao = reader.GetString(1)
+                        });
+                }
+                return listaTurmas;
+            }
+        }
+    }
+}
